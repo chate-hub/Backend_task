3503070925acf98b91ee757e5109322b3ee59b5f
chate-hub <chatebchilima20@gmail.com>
2023-04-26 22:30:18 +0200

    First Addition, Task left adding grapql and graphene to the platform

---
+++ notes/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('notes', views.NotesListView.as_view(), name="notes.list"),
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"),
+    path('notes/new', views.NotesCreateView.as_view(), name="notes.new"),
+    path('notes/<int:pk>/edit', views.NotesUpdateView.as_view(), name="notes.update"),
+    path('notes/<int:pk>/delete', views.NotesDeleteView.as_view(), name="notes.delete"),
+
+]
